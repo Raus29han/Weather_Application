29651c35d86804de45d0bb95b1716b10d0ba4e08
Raus29han <rk9698020@gmail.com>
2024-11-19 21:02:54 +0530

    Update Loading.js

--- Components/Loading-Page/Loading.js
+++ Components/Loading-Page/Loading.js
@@ -1,11 +1,10 @@
-import Image from 'next/image';
 import React from 'react';
 import "./Loading.css"
 
 const Loading = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <Image className='bounce' src={"/Public/Logo/Logo(1).png"} height={80} width={80}/>
+      <img className='bounce' src={"/Logo/Logo(1).png"} height={80} width={80}/>
     </div>
   );
 };
