8ef9276d9ed197b5fb1a59f7318e0ac32fc97e41
Raus29han <rk9698020@gmail.com>
2024-11-19 20:00:03 +0530

    Update Header.js

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -5,7 +5,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className='Logo bounce'>
-        <Image className='logo' src={"/Public/Logo/Logo(1).png"} height={100} width={100}/>
+        <Image className='logo' src={"Public/Logo/Logo(1).png"} height={100} width={100}/>
         <div className='text'>
             <h1>Weather</h1>
             <h3>forecast</h3>
