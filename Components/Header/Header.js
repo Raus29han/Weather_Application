0ac382e6db8ff2ff16e99c093b1058809e794b46
Raus29han <rk9698020@gmail.com>
2025-07-14 12:53:59 +0530

    Update Header.js
    add public in logo location

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className='Logo bounce'>
-        <img className='logo' src={"/Logo/Logo(1).png"} height={100} width={100}/>
+        <img className='logo' src={"Public/Logo/Logo(1).png"} height={100} width={100}/>
         <div className='text'>
             <h1>Weather</h1>
             <h3>forecast</h3>
