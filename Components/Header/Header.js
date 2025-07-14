c90a5fb28c535f3034f2bf1fcf33f8077a8ec9c1
Raus29han <rk9698020@gmail.com>
2025-07-14 13:02:56 +0530

    Update Header.js
    Changing img tag to Image

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Header.css";
+import Image from 'next/image';
 
 const Header = () => {
   return (
     <div className='Logo bounce'>
-        <img className='logo' src={"Public/Logo/Logo(1).png"} height={100} width={100}/>
+        <Image className='logo' src={"Public/Logo/Logo(1).png"} height={100} width={100}/>
         <div className='text'>
             <h1>Weather</h1>
             <h3>forecast</h3>
