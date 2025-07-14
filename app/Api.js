fcc05ee588369dcffb53f56b8fd90b619c5f050a
Raus29han <rk9698020@gmail.com>
2025-07-14 13:32:09 +0530

    Update Api.js
    changes in Api.js file

--- app/Api.js
+++ app/Api.js
@@ -1,17 +1,12 @@
-const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const GOI_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const CW_API = "https://api.openweathermap.org/data/2.5";
+
 export const goiApioptions = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': 'bf936a1928msh33f5c95d50a0b07p197150jsn13f733ad6ca9',
-		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+		'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
+		'x-rapidapi-host': process.env.NEXT_PUBLIC_RAPIDAPI_HOST
 	}
 };
 
-
-export const GOI_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
-
-export const CW_API = "https://api.openweathermap.org/data/2.5";
-export const API_KEY = "8e0243b075c2ca91bdf482f1aba3d61f";
-
-
-
+export const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
